d03d16c40d12ae0105e8008d2228a3e35d250ebf
alonmichu <alon.michu@gmail.com>
2022-04-17 21:18:51 +0300

    courier class improved: status->CourierStatus, +protected (_name,...), + checking work of class

--- courier.py
+++ courier.py
@@ -8,7 +8,7 @@ class Urgency(Enum):
     ONTIME = 3  # client chooses time
 
 
-class Status(Enum):
+class CourierStatus(Enum):
     FREE = 1  # свободен, как ветер, можно юзать
     DELIVERING = 2  # доставляет
     RETURNSBACK = 3  # возвращается
@@ -16,66 +16,76 @@ class Status(Enum):
 
 class Courier:
     def __init__(self, name, surname, age, cnt_order):
-        self.name = name
-        self.surname = surname
-        self.age = age
-        self.urgency = Urgency.ASAP
-        self.status = Status.FREE
-        # how much orders
-        self.cnt_order = cnt_order
+        self._name = name
+        self._surname = surname
+        self._age = age
+        self._urgency = Urgency.ASAP
+        self._status = CourierStatus.FREE
+        # how many orders
+        self._cnt_order = cnt_order
 
     @property
     def name(self):
-        return self.name
+        return self._name
 
     @property
     def surname(self):
-        return self.surname
+        return self._surname
 
     @property
     def age(self):
-        return self.age
+        return self._age
 
     @property
     def urgency(self):
-        return self.urgency
+        return self._urgency
 
     @property
     def status(self):
-        return self.status
+        return self._status
 
     @property
     def cnt_order(self):
-        return self.cnt_order
+        return self._cnt_order
 
     @name.setter
     def name(self, new_name):
-        self.name = new_name
+        self._name = new_name
 
     @surname.setter
     def surname(self, new_surname):
-        self.surname = new_surname
+        self._surname = new_surname
 
     @age.setter
     def age(self, new_age):
-        self.age = new_age
+        self._age = new_age
 
     @urgency.setter
     def urgency(self, new_urgency):
-        self.urgency = new_urgency
+        self._urgency = new_urgency
 
     @status.setter
     def status(self, new_status):
-        self.status = new_status
+        self._status = new_status
 
     @cnt_order.setter
     def cnt_order(self, new_cnt_order):
-        self.cnt_order = new_cnt_order
+        self._cnt_order = new_cnt_order
 
     def check_workload(self):
-        if (self.urgency == Urgency.ASAP or self.urgency == Urgency.ONTIME) \
-           and self.cnt_order < 5:
+        if (self._urgency == Urgency.ASAP
+            or self._urgency == Urgency.ONTIME) \
+           and self._cnt_order < 5:
             return True
-        if self.urgency == Urgency.URGENT and self.cnt_order == 1:
+        if self._urgency == Urgency.URGENT and self._cnt_order == 1:
             return True
         return False
+
+
+courier = Courier("Stepan", "Musorskiy", 34, 0)
+print("Hello! I am ", courier.name, " ", courier.surname, courier.age, "y.o.")
+print()
+courier.surname = "Mussorgsky"
+print("Hello! I am ", courier.name, " ", courier.surname, courier.age, "y.o.")
+print("Now my status is: ", courier.status)
+print("What's about urgency? They said ", courier.urgency)
