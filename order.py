9743a8996c45cc7b87eb2a47ed9d3219bfea4d33
kkrupinaa <79110379+kkrupinaa@users.noreply.github.com>
2022-04-27 08:42:04 +0300

    private prop to protected

--- order.py
+++ order.py
@@ -16,75 +16,75 @@ class Payment(Enum):  # Способы оплаты
 
 class Order:  # Заказ
     def __init__(self, order_id, promocode=None, product_list=[], payment=Payment.CARD):
-        self.__order_id = order_id  # ID заказа
-        self.__order_status = OrderStatus.NEW  # Статус заказа
-        self.__product_list = product_list  # Список ProductShopAvaliability
-        self.__starting_price = self.calculate_cost()  # Исходная цена заказа (без скидок)
-        self.__payment = payment  # Способ оплаты
-        self.__courier = None
-        self.__promocode = promocode  # Промокод
+        self._order_id = order_id  # ID заказа
+        self._order_status = OrderStatus.NEW  # Статус заказа
+        self._product_list = product_list  # Список ProductShopAvaliability
+        self._starting_price = self.calculate_cost()  # Исходная цена заказа (без скидок)
+        self._payment = payment  # Способ оплаты
+        self._courier = None
+        self._promocode = promocode  # Промокод
         if self.promocode is not None:  # Цена со скидкой
-            self.__total_price = self.apply_promocode()
+            self._total_price = self.apply_promocode()
         else:
-            self.__total_price = self.starting_price
+            self._total_price = self.starting_price
 
     # Геттеры
     @property
     def order_id(self):
-        return self.__order_id
+        return self._order_id
 
     @property
     def order_status(self):
-        return self.__order_status
+        return self._order_status
 
     @property
     def starting_price(self):
-        return self.__starting_price
+        return self._starting_price
 
     @property
     def total_price(self):
-        return self.__total_price
+        return self._total_price
 
     @property
     def payment(self):
-        return self.__payment
+        return self._payment
 
     @property
     def courier(self):
-        return self.__courier
+        return self._courier
 
     @property
     def promocode(self):
-        return self.__promocode
+        return self._promocode
 
     @property
     def product_list(self):
-        return self.__product_list
+        return self._product_list
 
     # Сеттеры
     @order_status.setter
     def order_status(self, new_order_status):
-        self.__order_status = new_order_status
+        self._order_status = new_order_status
 
     @starting_price.setter
     def starting_price(self, new_starting_price):
-        self.__starting_price = new_starting_price
+        self._starting_price = new_starting_price
 
     @total_price.setter
     def total_price(self, new_total_price):
-        self.__total_price = new_total_price
+        self._total_price = new_total_price
 
     @payment.setter
     def payment(self, new_payment):
-        self.__payment = new_payment
+        self._payment = new_payment
 
     @courier.setter
     def courier(self, new_courier):
-        self.__courier = new_courier
+        self._courier = new_courier
 
     @promocode.setter
     def promocode(self, new_promocode):
-        self.__promocode = new_promocode
+        self._promocode = new_promocode
 
     def calculate_cost(self):
         result = 0
